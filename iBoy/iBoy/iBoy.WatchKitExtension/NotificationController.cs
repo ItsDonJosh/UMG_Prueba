7502b52c9e99c2d543d101c9a31eb2bfa77b2546
José Mario <Jos Mario@DONJOSH-PC>
2016-05-21 22:47:28 -0600

    Cambio de texto

--- iBoy/iBoy/iBoy.WatchKitExtension/NotificationController.cs
+++ iBoy/iBoy/iBoy.WatchKitExtension/NotificationController.cs
@@ -16,7 +16,7 @@ namespace iBoy.WatchKitExtension
             base.Awake(context);
 
             // Configure interface objects here.
-            Console.WriteLine("{0} awake with context", this);
+            Console.WriteLine("{0} Aquí se esta modificando", this);
         }
 
         public override void WillActivate()
