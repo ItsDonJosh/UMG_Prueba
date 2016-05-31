4d56c022a41d1ae29845d1318f0850eeab1588e4
José Mario <Jos Mario@DONJOSH-PC>
2016-05-30 22:35:22 -0600

    Cambio de texto

--- iBoy/iBoy/iBoy.WatchKitExtension/GlanceController.cs
+++ iBoy/iBoy/iBoy.WatchKitExtension/GlanceController.cs
@@ -16,7 +16,7 @@ namespace iBoy.WatchKitExtension
             base.Awake(context);
 
             // Configure interface objects here.
-            Console.WriteLine("{0} awake with context", this);
+            Console.WriteLine("{0}Hola, bienvenido", this);
         }
 
         public override void WillActivate()
